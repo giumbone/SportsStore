8b7f62f48d287c3447b93e7041eaab78ff3b3244
giumbone <johnnydeep29485@gmail.com>
2024-05-31 16:01:39 +0300

    Initialized database.py

---
+++ database.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///mydatabase.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    delivery_address = db.Column(db.Text, nullable=False)
+    product = db.relationship('Product', backref=db.backref('orders', lazy=True))
+
+def init_db():
+  db.create_all()
+
+if __name__ == '__main__':
+    init_db()
