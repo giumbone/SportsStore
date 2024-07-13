79e5949d4dcf42ead703103b93721461e4165150
giumbone <johnnydeep29485@gmail.com>
2024-07-13 20:56:23 +0300

    added new function

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -28,16 +28,49 @@ const CheckoutForm: React.FC = () => {
     cvv: '',
   });
 
+  const [errors, setErrors] = useState<PaymentInformation>({
+    cardNumber: '',
+    expiryDate: '',
+    cvv: '',
+  });
+
+  const validateForm = () => {
+    const errors: PaymentInformation = { cardNumber: '', expiryDate: '', cvv: '' };
+    if (!/^\d{16}$/.test(paymentInformation.cardNumber)) {
+      errors.cardNumber = 'Invalid card number';
+    }
+
+    if (!/^\d{2}\/\d{2}$/.test(paymentInformation.expiryDate)) {
+      errors.expiryDate = 'Invalid expiry date format';
+    }
+
+    if (!/^\d{3}$/.test(paymentInformation.cvv)) {
+      errors.cvv = 'Invalid CVV';
+    }
+
+    setErrors(errors);
+    return Object.values(errors).every((x) => x === '');
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setState: React.Dispatch<React.SetStateAction<any>>) => {
     const { name, value } = e.target;
     setState((prevState: any) => ({
       ...prevState,
       [name]: value,
     }));
+    setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: '',
+    }));
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!validateForm()) {
+      alert('Please correct the errors before submitting.');
+      return;
+    }
+    
     try {
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/orders`, {
         deliveryAddress,
@@ -66,15 +99,18 @@ const CheckoutForm: React.FC = () => {
       ))}
       <h2>Payment Information</h2>
       {Object.keys(paymentInformation).map((key) => (
-        <input
-          key={key}
-          name={key}
-          type={key === 'cvv' ? 'password' : 'text'}
-          value={(paymentInformation as any)[key]}
-          onChange={(e) => handleChange(e, setPaymentInformation)}
-          placeholder={key.charAt(0).toUpperCase() + key.slice(1)}
-          required
-        />
+        <div key={key}>
+          <input
+            name={key}
+            type={key === 'cvv' ? 'password' : 'text'}
+            value={(paymentInformation as any)[key]}
+            onChange={(e) => handleChange(e, setPayment Scholarly Input>
+            placeholder={key.charAt(0).toUpperCase() + key.slice(1)}
+            required
+            style={errors[key] ? { borderColor: 'red' } : {}}
+          />
+          {errors[key] && <p style={{ color: 'red' }}>{errors[key]}</p>}
+        </div>
       ))}
       <button type="submit">Submit Order</button>
     </form>
